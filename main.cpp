5367f761b10e32e92bbe6783571c79576b14dda1
elrm921 <pocketrocket03@gmail.com>
2022-12-18 01:30:39 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -2,21 +2,23 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <variant>
 #include <algorithm>
 #include <regex>
 
 using namespace std;
-typedef pair<string,string> kvalue;
-typedef pair<string,vector<kvalue>> vdata;
+typedef variant<int,double,bool,string,monostate> value;
+typedef pair<string,value> kvalue;
+typedef pair<string,vector<kvalue>> vecdata;
 
 class ini_parser
 {
     string src;
-    vector<vdata> m_data;
+    vector<vecdata> m_data;
 public:
     ini_parser(const string& name) : src(name)
     {
-        vector<vdata>::iterator current_section;
+        vector<vecdata>::iterator current_section;
         ifstream source_data(src);
         if (!source_data.is_open()) throw logic_error("No such file " + src);
         while (source_data.is_open() && !source_data.eof())
@@ -32,7 +34,7 @@ public:
                 line = line.substr(1,line.size()-2);
                 if (find_data_section(m_data, line) == m_data.end())
                 {
-                    vdata section;
+                    vecdata section;
                     section.first = line;
                     m_data.push_back(section);
                 }
@@ -44,14 +46,47 @@ public:
             {
                 size_t split = line.find('=',0);
                 string key = line.substr(0,split);
-                string value = line.substr(split+1);
+                string svalue = line.substr(split+1);
+                double dvalue = 0; int ivalue = 0; bool bvalue = 0;
+                enum value_type { boolean, integer, floting, string, empty };
+                int type;
 
-                kvalue this_pair(key,value);
+                if (regex_search(svalue, regex("^\\d+\\.\\d+", regex::ECMAScript)))
+                {
+                    type = value_type::floting;
+                    dvalue = stod(svalue);
+                }
+                else if (regex_search(svalue, regex("^\\d+", regex::ECMAScript)))
+                {
+                    type = value_type::integer;
+                    ivalue = stoi(svalue);
+                }
+                else if (regex_search(svalue, regex("[A-zА-я]+", regex::ECMAScript)))
+                {
+                    type = value_type::string;
+                }
+                else
+                {
+                    type = value_type::empty;
+                }
+
                 auto it = find_section_key(current_section->second, key);
                 if (it == current_section->second.end())
+                {
+                    kvalue this_pair;
+                    this_pair.first = key;
+                    this_pair.second = monostate();
+                    if (type == value_type::integer) this_pair.second = ivalue;
+                    if (type == value_type::floting) this_pair.second = dvalue;
+                    if (type == value_type::string) this_pair.second = svalue;
                     current_section->second.push_back(this_pair);
+                }
                 else
-                    it->second = value;
+                {
+                    if (type == value_type::integer) it->second = ivalue;
+                    if (type == value_type::floting) it->second = dvalue;
+                    if (type == value_type::string) it->second = svalue;
+                }
             }
         }
     }
@@ -67,11 +102,11 @@ public:
         vector<kvalue> keys = it1->second;
         auto it2 = find_section_key(keys,skey);
         if (it2 == keys.end()) throw logic_error("Key " + skey + " does not exist");
-        T res = it2->second;
+        T res = get<T>(it2->second);
         return res;
     }
 
-    vector<vdata>::iterator find_data_section(vector<vdata> &data, string &name)
+    vector<vecdata>::iterator find_data_section(vector<vecdata> &data, string &name)
     {
         auto it = data.begin();
         while (it != data.end())
@@ -95,6 +130,7 @@ public:
         return it; 
     }
 
+    template<typename T>
     void print_data()
     {
         for (auto &p : m_data)
@@ -102,7 +138,8 @@ public:
             cout << p.first << endl;
             for (auto &v : p.second)
             {
-                cout << v.first << "=" << v.second << endl;
+                if (holds_alternative<T>(v.second))
+                    cout << v.first << "=" << get<T>(v.second) << endl;
             }
         }
     }
@@ -113,11 +150,14 @@ int main()
     try
     {
         ini_parser parser("example.ini");
-        // parser.print_data();
-        auto value = parser.get_value<string>("Section1.var1");
+        auto value = parser.get_value<double>("Section1.var1");
         cout << "get_value::section1::var1 " << value << endl;
     }
     catch(const logic_error& e)
+    {
+        std::cerr << e.what() << endl;
+    }
+    catch(const bad_variant_access& e)
     {
         std::cerr << e.what() << endl;
     }
